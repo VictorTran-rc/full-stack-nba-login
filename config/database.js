5e982f5c68aa289f4094ad1c3c555893745da9a8
Victor Tran <60512497+VictorTran-rc@users.noreply.github.com>
2020-03-21 15:51:15 -0400

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://nba:cool@cluster0-d1h5k.mongodb.net/test?retryWrites=true&w=majority', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'nba'
+};
